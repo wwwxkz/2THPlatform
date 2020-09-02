5432375333f0f1ffb970ece1192e82bdb9a08abb
wwwxkz <wxkzwin@gmail.com>
2020-09-02 12:02:01 -0300

    send method now sends data to database

--- 2THPlatform/api/v1/calls/report.php
+++ 2THPlatform/api/v1/calls/report.php
@@ -25,15 +25,41 @@
         public function send($parameters)
 		{
             $data = explode('_', $parameters);
+            // DEBUG: Show parameters
             echo "Mac: " . $data[0] . "\n";
             echo "Lat: " . $data[1] . "\n";
             echo "Lon: " . $data[2] . "\n";
 
-            // Send to database
-			//$con = new PDO('mysql: host=locahost; dbname=company;','root','');
+            if(strlen($data[0]) == 12) {
+                $con = new PDO('mysql: host=locahost; dbname=company;','root','');
 
-			//$sql = "SELECT * FROM reports ORDER BY id ASC"; // Create new position in database related to macadress
-			//$sql = $con->prepare($sql);
-			//$sql->execute();
+                $sql = "SELECT * FROM `reports` WHERE `mac`='" . $data[0] . "'";
+                $sql = $con->prepare($sql);
+                $sql->execute();
+
+                if ($sql->fetch(PDO::FETCH_ASSOC) == true){
+                    echo 'Exist';  
+                    try {   
+                        $sql = "UPDATE `reports` SET `lat`=" . $data[1] . ",`lon`=" . $data[2] . ",`date`=" . date("Y-m-d") . " WHERE `mac`='" . $data[0] . "'";
+                        $con->exec($sql);
+                        echo "Record updated successfully";
+                    } catch(PDOException $e) {
+                        echo $sql . "<br>" . $e->getMessage();
+                    }
+                } else {   
+                    echo 'Does not exist';
+                    try {
+                        $sql = "INSERT INTO `reports`(`mac`, `lat`, `lon`, `date`) VALUES (" . $data[0] . "," . $data[1] . "," . $data[2] . "," . date("Y-m-d") . ")";
+                        $con->exec($sql);
+                        echo "New report created successfully";
+                    } catch(PDOException $e) {
+                        echo $sql . "<br>" . $e->getMessage();
+                    }
+                }
+                $con = null;
+            } else {
+                throw new Exception("This is not a MAC Address");
+            }
+
 		}
 	}
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # 2THPlatform
 
+base url:
+http://localhost/2THPlatform
+
+classes:
+http://localhost/2THPlatform/api/v1/report/
+
+methods:
+http://localhost/2THPlatform/api/v1/report/send/
+http://localhost/2THPlatform/api/v1/report/get/
+
+# sending data to API 
+
+MAC:0088144D4CFB
+_
+LAT:-74.0059731
+_
+LON:40.77222188
+
+0088144D4CFB_-74.0059731_40.77222188
+
+http://localhost/2THPlatform/api/v1/report/send/0088144D4CFB_-74.0059731_40.77222188
+
+# database
+
+company / reports 
+
+id - autoincrement
+mac - varchar(12)
+lat - int
+lon - int
+date - date
+name - varchar(64)
+tag - varchar(32)
+groups - varchar(128)
