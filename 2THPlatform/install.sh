04523cbfa086b80a76fecf28eedf8f72ffd530a0
wwwxkz <wxkzwin@gmail.com>
2022-07-11 09:07:07 -0300

    Removed install and uninstall commands from script

--- 2THPlatform/install.sh
+++ 2THPlatform/install.sh
@@ -6,55 +6,23 @@
 # Get parameters when script is invoke
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -d|--dir) dir="$2"; shift ;;
-        -i|--install) option=1 ;;
-	-u|--uninstall) option=2 ;;
-	-r|--reinstall) option=3 ;;
-	-h|--help) option=4 ;;
-	-c|--create) option=5 ;;
-	-p|--perish) option=6 ;;
-	-a|--add-users) option=7 ;;
-	-s|--drop-users) option=8 ;;
+	-h|--help) option=1 ;;
+	-c|--create) option=2 ;;
+	-p|--perish) option=3 ;;
+	-a|--add-users) option=4 ;;
+	-s|--drop-users) option=5 ;;
         *) echo "\n-> Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
 # Functions
-function _install {
-	cd $1
-	git clone https://github.com/wwwxkz/2THPlatform.git
-	git clone https://github.com/wwwxkz/2THApi.git
-	mkdir api
-	mv 2THApi/* api/
-	rm -rf 2THApi/
-	rm api/README.md
-	rm 2THPlatform/README.md
-	echo -e '\n-> Success'
-}
-
-function _uninstall {
-	rm -rf 2THPlatform/
-	rm -rf 2THApi/
-	rm -rf api/
-}
-
 function _help {
 	echo -e '\n-> Help';
-	echo -e '\n- Install: -i or --install';
-	echo '- Uninstall: -u or --uninstall';
-	echo '- Reinstall: -r or --reinstall';
-	echo '- Install dir: -d or --dir';
 	echo '- Create: -c or --create';
 	echo '- Perish: -p or --perish';
 	echo '- Add users: -a or --add-users';
 	echo '- Drop users: -s or --drop-users';
-	echo -e '\n-> Usage:';
-	echo -e '\n- ./install.sh -i';
-	echo '- ./install.sh -i -d /var/www/folder/';
-	echo '- ./install.sh -u -d /var/www/folder/';
-	echo -e '\n-> Notes:';
-	echo -e '\n- If installed with -d, uninstall using -d\n';
 }
 
 function _create_db {
@@ -86,13 +54,10 @@ else
 	# Now check which function to execute
 	case $option in
 		# Use dir specified
-		1) _install $dir;;
-		2) _uninstall;;
-		3) _uninstall; _install;;
-		4) _help;;
-		5) _create_db;;
-		6) _drop_db;;
-		7) _add_users;;
-		8) _drop_users;;
+		1) _help;;
+		2) _create_db;;
+		3) _drop_db;;
+		4) _add_users;;
+		5) _drop_users;;
 	esac
 fi
